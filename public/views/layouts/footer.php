499175f3554508261047dc0b36a7d7e582002028
Nebulatools <n3bul4tools@gmail.com>
2025-09-08 15:08:08 -0600

    Update 100% - Footer

--- public/views/layouts/footer.php
+++ public/views/layouts/footer.php
@@ -1,36 +1,36 @@
         <footer class="footer">
-            <div class="footer-content">
-                <div class="footer-left">
-                    <div class="footer-about">
-                        <div class="footer-header">
-                            <h3 class="footer-title">Gallery & Framing Studio</h3>
-                            <p class="footer-address">Av. José Vasconcelos 621</p>
+            <div class="footer-content-1">
+                <div class="footer-left-1">
+                    <div class="footer-about-1">
+                        <div class="footer-header-1">
+                            <h3 class="footer-title-1">Gallery & Framing Studio</h3>
+                            <p class="footer-address-1">Av. José Vasconcelos 621</p>
                         </div>
-                        <p class="footer-description">
+                        <p class="footer-description-1">
                             We forge alliances with modern artists<br>
                             from all corners of the world to create<br>
                             unparalleled masterpieces of the utmost caliber.
                         </p>
                     </div>
-                    <p class="footer-copyright">&copy; <?= date('Y'); ?> <?= SITE_NAME; ?>. All rights reserved.</p>
                 </div>
-                <div class="footer-right">
-                    <div class="footer-links">
-                        <div class="footer-link-group">
-                            <a href="<?= BASE_URL; ?>catalogue" class="footer-link">FRAMING STUDIO</a>
-                            <a href="<?= BASE_URL; ?>artworks" class="footer-link">ARTWORKS</a>
-                            <a href="<?= BASE_URL; ?>artists" class="footer-link">ARTISTS</a>
-                            <a href="<?= BASE_URL; ?>gallery" class="footer-link">GALLERY</a>
+                <div class="footer-right-1">
+                    <div class="footer-links-1">
+                        <div class="footer-link-group-1">
+                            <a class="footer-link-1" href="<?= BASE_URL; ?>catalogue">FRAMING STUDIO</a>
+                            <a class="footer-link-1" href="<?= BASE_URL; ?>artworks">ARTWORKS</a>
+                            <a class="footer-link-1" href="<?= BASE_URL; ?>artists">ARTISTS</a>
+                            <a class="footer-link-1" href="<?= BASE_URL; ?>gallery">GALLERY</a>
                         </div>
-                        <div class="footer-link-group">
-			    <a href="<?= BASE_URL; ?>how-we-do-it" class="footer-link">ABOUT</a>
-                            <a href="#" class="footer-link">FAQ</a>
-                            <a href="#" class="footer-link">CONTACT</a>
-                            <a href="#" class="footer-link">INSTAGRAM</a>
+                        <div class="footer-link-group-1">
+                            <a class="footer-link-1" href="<?= BASE_URL; ?>how-we-do-it">ABOUT</a>
+                            <a class="footer-link-1" href="#">FAQ</a>
+                            <a class="footer-link-1" href="#">CONTACT</a>
+                            <a class="footer-link-1" href="#">INSTAGRAM</a>
                         </div>
                     </div>
                 </div>
             </div>
+            <p class="footer-copyright-1">&copy; <?= date('Y'); ?> <?= SITE_NAME; ?>. All rights reserved.</p>
         </footer>
     </body>
 </html>
