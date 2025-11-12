69c4d8b415b2ac2bbcb65fe482071a678fc84e79
Nebulatools <n3bul4tools@gmail.com>
2025-11-12 08:20:57 -0600

    Ajustes "About"

--- public/views/about.php
+++ public/views/about.php
@@ -8,49 +8,44 @@
         <div class="dual-grid-20">
             <div class="dual-left-column-20">
                 <div class="dual-content-20">
-                    <div class="dual-header-20"></div>
                     <div class="left-description-20">
-                        <p class="about-subtitle-20">Bespoke Artvisory</p>
-                        <br>
-                        <p class="left-text-20">
-                            ARTRA exists in the space between art and emotion.<br>
-                            We frame with perspective&#8212; shaping how art lives<br>
-                            in its environment, how it breathes with light, with<br>
-                            space&#8212; how it meets the eye.<br>
-                            Every decision is deliberate: proportion, texture, color.
-                        </p>
+                        ARTRA was born from the need to honor what art
+                        makes us feel &#8212;from the belief that a frame is
+                        not a boundary, but a continuation of the story.<br>
+                        Think sculptural textures, deep tones, and bold
+                        simplicity&#8212; frames that do more than surround art;
+                        they complete it.
                     </div>
                 </div>
             </div>
             <div class="dual-right-column-20">
-                <picture>
-                    <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-03.png" media="(max-width: 767px)">
-                    <img class="dual-right-image-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-03.png" alt="Framed Work">
-                </picture>
+                <div class="dual-right-image-20">
+                    <picture>
+                        <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-02.png" media="(max-width: 767px)">
+                        <img class="dual-img-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-02.png" alt="Framed Work">
+                    </picture>
+                </div>
             </div>
         </div>
     </section>
     <section class="dual-section-20">
         <div class="dual-grid-20">
             <div class="dual-left-column-21">
-                <picture>
-                    <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-04.jpg" media="(max-width: 767px)">
-                    <img class="dual-left-image-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-04.jpg" alt="Street style with sneakers">
-                </picture>
+                <div class="dual-left-image-20">
+                    <picture>
+                        <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-03.png" media="(max-width: 767px)">
+                        <img class="dual-img-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-03.png" alt="Street style with sneakers">
+                    </picture>
+                </div>
             </div>
             <div class="dual-right-column-21">
                 <div class="dual-content-20">
-                    <div class="dual-header-20"></div>
                     <div class="right-description-20">
-                        <p class="right-text-20">
-                            ARTRA was born from the need to honor what art makes us<br>
-                            feel. From the belief that a frame is not a boundary, but a<br>
-                            continuation of the story.<br>
-                            It shapes how a piece is seen, how it lives in its space, and<br>
-                            how it moves those who encounter it. Because every frame<br>
-                            tells a story: of perspective, of presence,<br>
-                            of dialogue between art and its surroundings.
-                        </p>
+                        ARTRA exists in the space between art and emotion.<br>
+                        We frame with perspective &#8212;shaping how art
+                        lives in its environment, how it breathes with light,
+                        with space&#8212; how it meets the eye.
+                        Every decision is deliberate: proportion, texture, color.
                     </div>
                 </div>
             </div>
@@ -60,34 +55,35 @@
         <div class="dual-grid-20">
             <div class="dual-left-column-20">
                 <div class="dual-content-20">
-                    <div class="dual-header-20"></div>
                     <div class="left-description-20">
-                        <p class="left-text-20">
-                            We believe framing si a form of storytelling - a dialogue<br>
-                            between craft, color and emotion.<br>
-                            Every piece deserves a frame that understands it &#8212; its<br>
-                            rhythm, its quietness or loudness, its intent.
-                            <br><br>
-                            Our process is slow, deliberate and deeply human.<br>
-                            <i>Measured by eye guided by feeling.</i>
-                            <br><br>
-                            We design with emotion and precision, shaping not just<br>
-                            how art its seen , but how it lives within a space.<br>
-                            To frame is to listen &#8212; to the art, the room the person<br>
-                            behind it.
-                            <br><br><br>
-                            BECAUSE PRESENTATION IS NOT JUST AN<br>
-                            AFTERTHOUGHT &#8212; ITS THE FINAL ACT OF<br>
-                            CREATION.
-                        </p>
+                        We believe framing si a form of storytelling &#8212;
+                        a dialogue between craft, color and emotion.<br>
+                        Every piece deserves a frame that understands it &#8212;
+                        its rhythm, its quietness or loudness, its intent.<br>
+                        Our process is slow, deliberate and deeply human &#8212;
+                        measured by eye, guided by feeling.
+                        <br><br>
+                        <i>
+                            We design with emotion and precision,
+                            shaping not just how art its seen,
+                            but how it lives within a space.<br>
+                            To frame is to listen &#8212; to the art,
+                            the room the person behind it.
+                        </i>                          
+                        <br><br><br>
+                        BECAUSE PRESENTATION IS NOT JUST AN
+                        AFTERTHOUGHT &#8212; ITS THE FINAL ACT OF
+                        CREATION.
                     </div>
                 </div>
             </div>
             <div class="dual-right-column-20">
-                <picture>
-                    <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-05.png" media="(max-width: 767px)">
-                    <img class="dual-right-image-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-05.png" alt="Framed Work">
-                </picture>
+                <div class="dual-right-image-20">
+                    <picture>
+                        <source srcset="<?= BASE_URL; ?>/public/assets/image/about/img-04.png" media="(max-width: 767px)">
+                        <img class="dual-img-20" src="<?= BASE_URL; ?>/public/assets/image/about/img-04.png" alt="Framed Work">
+                    </picture>
+                </div>
             </div>
         </div>
     </section>
