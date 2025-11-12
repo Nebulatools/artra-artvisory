6e7b64e7008702aa736350bf2235ea906485108b
Nebulatools <n3bul4tools@gmail.com>
2025-11-12 09:09:11 -0600

    Ajuste página "How We Do It"

--- public/views/hwdi.php
+++ public/views/hwdi.php
@@ -17,42 +17,51 @@
             <div class="dual-left-column-30">
                 <div class="dual-content-30">
                     <div class="left-description-30">
-                        <p class="left-text-30">
-                            Our process is both intuitive and methodical.<br>
-                            From concept sketch to final installation.<br>
-                            We prototype, test and refine- balancing color, grain and scale&#8212;<br>
-                            every line reconsidered until it feels right, until the piece feels
-                            inevitable.
-                            <br><br>
-                            <i>Like it always belonged there.</i>
-                        </p>
+                        Our process is both intuitive and methodical &#8212;
+                        from concept sketch to final installation.<br>
+                        We prototype, test and refine, balancing color,
+                        grain and scale.
+                        <br><br>
+                        <i>
+                            Every line reconsidered until it feels right &#8212;
+                            until the piece feels inevitable,
+                            as if it always belonged together.
+                        </i>
                     </div>
                 </div>
             </div>
             <div class="dual-right-column-30">
-                <picture>
-                    <source srcset="<?= BASE_URL; ?>/public/assets/image/hwdi/img-03.png" media="(max-width: 767px)">
-                    <img class="dual-right-image-30" src="<?= BASE_URL; ?>/public/assets/image/hwdi/img-03.png" alt="Framed Work">
-                </picture>
+                <div class="dual-right-image-30">
+                    <picture>
+                        <source srcset="<?= BASE_URL; ?>/public/assets/image/hwdi/img-03.png" media="(max-width: 767px)">
+                        <img class="dual-img-30" src="<?= BASE_URL; ?>/public/assets/image/hwdi/img-03.png" alt="Framed Work">
+                    </picture>
+                </div>
             </div>
         </div>
     </section>
     <section class="dual-section-30">
         <div class="dual-grid-30">
             <div class="dual-left-column-31">
-                <picture>
-                    <source srcset="<?= BASE_URL; ?>/public/assets/image/hwdi/img-04.png" media="(max-width: 767px)">
-                    <img class="dual-left-image-30" src="<?= BASE_URL; ?>/public/assets/image/hwdi/img-04.png" alt="Street style with sneakers">
-                </picture>
+                <div class="dual-left-image-30">
+                    <picture>
+                        <source srcset="<?= BASE_URL; ?>/public/assets/image/hwdi/img-04.png" media="(max-width: 767px)">
+                        <img class="dual-img-30" src="<?= BASE_URL; ?>/public/assets/image/hwdi/img-04.png" alt="Street style with sneakers">
+                    </picture>
+                </div>
             </div>
             <div class="dual-right-column-31">
                 <div class="dual-content-30">
                     <div class="right-description-30">
-                        <p class="right-text-30">
-                            What we create is not decoration but dialogue&#8212; between<br>
-                            craft and concepts, between hand and material, between<br>
-                            what's seen and what's felt.
-                        </p>
+                        Detail by detail, we step back until only
+                        the art remains.<br>
+                        Perfect alignment, subtle shadow,
+                        a weightless presence.
+                        <br><br>
+                        <i>
+                            Our job is to let the work stand out &#8212;
+                            with the clarity and grace it deserves.
+                        </i>
                     </div>
                 </div>
             </div>
